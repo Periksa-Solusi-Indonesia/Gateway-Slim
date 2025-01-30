8bc84c0e82d110ab6865fa2efacd74f2e3c069b8
Sutan Imam Abu Hanifah <sutanimam@windowslive.com>
2025-01-30 12:43:12 +0700

    update

--- public/index.php
+++ public/index.php
@@ -11,8 +11,8 @@ use Slim\Factory\AppFactory;
 use Slim\Factory\ServerRequestCreatorFactory;
 use Dotenv\Dotenv;
 
-require 'vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 // Instantiate PHP-DI ContainerBuilder
